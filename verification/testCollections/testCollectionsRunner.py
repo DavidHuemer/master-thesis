c352d92ad8244223bd115e665b74de68b13d0674
DavidHuemer <92david.huemer@gmail.com>
2024-09-18 19:43:31 +0200

    Added testCollectionsRunner

---
+++ verification/testCollections/testCollectionsRunner.py
@@ -0,0 +1,31 @@
+from definitions.ast.jmlTreeNode import JmlTreeNode
+from definitions.codeExecution.runtime.javaRuntimeClass import JavaRuntimeClass
+from definitions.evaluations.tests.testCollections import TestCollections
+from definitions.inconsistencyTestCase import InconsistencyTestCase
+from definitions.verification.verificationResult import VerificationResult
+from helper.logs.loggingHelper import LoggingHelper
+from verification.result.verificationResultFactory import VerificationResultFactory
+from verification.testCase.testCaseRunner import TestCaseRunner
+
+
+class TestCollectionsRunner:
+    def __init__(self, test_case_runner=TestCaseRunner()):
+        self.test_case_runner = test_case_runner
+
+    def run(self, test_class: JavaRuntimeClass, test_collections: TestCollections,
+            inconsistency_test_case: InconsistencyTestCase, ast: JmlTreeNode) -> VerificationResult:
+        LoggingHelper.log_info("Running test collections")
+
+        # Steps to run test collections
+        # 1. Run positive tests
+        for test_case in test_collections.test_collection.test_cases:
+            result = self.test_case_runner.run(test_class, test_case, inconsistency_test_case=inconsistency_test_case,
+                                               ast=ast)
+
+            if not result:
+                return VerificationResultFactory.inconsistent_result(inconsistency_test_case)
+
+        # 2. Run exception tests
+        # TODO: Run exception tests
+
+        return VerificationResultFactory.consistent_result(inconsistency_test_case)
