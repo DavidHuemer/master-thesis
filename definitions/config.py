31cd122f3c76c587b63673c53c185da54468e02f
DavidHuemer <92david.huemer@gmail.com>
2024-09-18 19:58:09 +0200

    Updated config

--- definitions/config.py
+++ definitions/config.py
@@ -1,3 +1,17 @@
 LOGGER_NAME = 'master-thesis-logger'
+LOGGER_WITHOUT_LEVEL_NAME = 'master-thesis-logger-without-level'
+
 EXPECTED_RESULTS_LOCATION = 'data/expected_results.txt'
 CODE_DIRECTORY = 'data/code'
+API_KEY_PATH = 'secrets/API_KEY.txt'
+MAX_PIPELINE_TRIES = 1
+JML_TRANSFORMATIONS_EXAMPLES_FILE_PATH = 'data/examples/jml_transformations_examples.txt'
+JML_TRANSFORMATIONS_EXAMPLES_COUNT = 3
+JML_CONTEXT = 'You are a JML (Java Modeling Language) expert. I will give you a JavaDoc comment and you will generate the JML specification for the method.'
+OPEN_AI_MODEL = 'gpt-3.5-turbo'
+ANTLR_JAR_PATH = 'C:\\FH\\Master\\Masterarbeit\\tools\\antlr-4.13.2-complete.jar'
+ANTLR_COMMAND = 'org.antlr.v4.Tool'
+PARSER_FILE = 'JML.g4'
+POSITIVE_JML_PARSER_FILE = 'data/examples/positive_jml_parser_examples.txt'
+
+CODE_DIST_FOLDER = 'dist'
